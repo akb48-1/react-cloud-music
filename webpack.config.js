c73dccdb839a148db5e3b65c0630ecee1bdb875e
123456+1s <akb48_1314520@163.com>
2018-03-06 07:41:39 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const ROOT = path.resolve(__dirname);
+
 module.exports = {
     output: {
         filename: '[name].bundle.[hash:4].js',
-        path: ROOT + '/dist/'
+        path: ROOT + '/dist/',
         publicPath: '/',
     },
     externals: {
